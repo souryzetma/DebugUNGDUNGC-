1a0c0411de0849daecac9fb4ebfd32b746c552eb
unknown <souryzetma1144@yahoo.com>
2024-08-16 19:57:09 +0700

    commit 1

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Draw
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                System.Console.WriteLine("Index= "+ i) ;
+            }
+        }
+    }
+}
